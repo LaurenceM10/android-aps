ec3b8468eb89deab15ff42f70d48200b73fa7b3f
LaurenceM10 <laurenmontenegro10@gmail.com>
2018-04-02 15:51:06 -0600

    Cleaned base code

---
+++ app/src/main/java/techo/apps/isi/uca/com/android_aps/models/ExperienceModel.java
@@ -0,0 +1,9 @@
+package techo.apps.isi.uca.com.android_aps.models;
+
+/**
+ * Created by macyarin on 2/4/18.
+ */
+
+public class ExperienceModel {
+    //TODO: verify the correct fields of this model
+}
--- app/src/main/java/techo/apps/isi/uca/com/android_aps/ui/activities/MainActivity.java
+++ app/src/main/java/techo/apps/isi/uca/com/android_aps/ui/activities/MainActivity.java
@@ -2,8 +2,6 @@ package techo.apps.isi.uca.com.android_aps.ui.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,41 +10,71 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import techo.apps.isi.uca.com.android_aps.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar toolbar;
+    private FloatingActionButton floatingActionButton;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //Call all the necessary methods
+        initViews();
+        configureViewElements();
+        initActions();
+    }
+
+
+    /**
+     * To get references of the view elements
+     */
+    private void initViews() {
+        toolbar = findViewById(R.id.toolbar);
+        floatingActionButton = findViewById(R.id.fab);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+    }
+
+    /**
+     * To initiate actions when events occur on the elements
+     */
+    private void initActions() {
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //TODO: create an action in dependency of the context
             }
         });
+    }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    /**
+     * To configure some elements of the activity
+     */
+    private void configureViewElements() {
+        //set the support for the action bar
+        setSupportActionBar(toolbar);
+
+        //Configure the ActionBarDrawerToggle
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        //Setup the navigation view
         navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -96,7 +124,7 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
