e58acebcef0965e50031f5553b74f95b5b5fb7fe
LaurenceM10 <laurenmontenegro10@gmail.com>
2018-04-03 09:08:57 -0600

    Cleaned Navigation Drawer

--- app/src/main/java/techo/apps/isi/uca/com/android_aps/ui/activities/MainActivity.java
+++ app/src/main/java/techo/apps/isi/uca/com/android_aps/ui/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package techo.apps.isi.uca.com.android_aps.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -110,18 +111,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_logout) {
+            startActivity(new Intent(this, LoginActivity.class));
+            this.finish();
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
