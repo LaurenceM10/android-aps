a40352324f17dc7c1793fa81afc070a999d83980
LaurenceM10 <laurenmontenegro10@gmail.com>
2018-04-10 10:41:18 -0600

    Retrofit configuration to make API requests

---
+++ app/src/main/java/techo/apps/isi/uca/com/android_aps/api/Api.java
@@ -0,0 +1,25 @@
+package techo.apps.isi.uca.com.android_aps.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by macyarin on 10/4/18.
+ */
+
+public class Api {
+    private final static String URL = "";
+
+    public static String getBase() {
+        return URL + "";
+    }
+
+    public static ApiInterface instance() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.getBase())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return retrofit.create(ApiInterface.class);
+    }
+}
---
+++ app/src/main/java/techo/apps/isi/uca/com/android_aps/api/ApiInterface.java
@@ -0,0 +1,9 @@
+package techo.apps.isi.uca.com.android_aps.api;
+
+/**
+ * Created by macyarin on 10/4/18.
+ */
+
+public interface ApiInterface {
+    //Make all the request to the endpoints
+}
