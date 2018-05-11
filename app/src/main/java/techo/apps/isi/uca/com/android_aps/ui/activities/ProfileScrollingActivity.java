924c14176ea79b18ed2c33c792bab7dd332422de
Lauren <laurenmontenegro10@gmail.com>
2018-05-10 20:42:48 -0600

    Changed style in message button and added ProfileActivity

---
+++ app/src/main/java/techo/apps/isi/uca/com/android_aps/ui/activities/ProfileScrollingActivity.java
@@ -0,0 +1,30 @@
+package techo.apps.isi.uca.com.android_aps.ui.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import techo.apps.isi.uca.com.android_aps.R;
+
+public class ProfileScrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
